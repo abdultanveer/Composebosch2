6127941176557940b20e1459756dba4018625f54
abdul <abdul.tanveer@gmail.com>
2025-03-11 17:47:26 +0530

    sms broadcastreceiver-retreive the sms content

--- app/src/main/java/com/example/composebosch/SmsMsgReceiver.kt
+++ app/src/main/java/com/example/composebosch/SmsMsgReceiver.kt
@@ -3,11 +3,15 @@ package com.example.composebosch
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.provider.Telephony
+import android.telephony.SmsMessage
 import android.util.Log
 
 class SmsMsgReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
-        Log.i("smsmsgreceiver","we got a sms after rally app")
-    }
+        if (intent?.action.equals(Telephony.Sms.Intents.SMS_RECEIVED_ACTION)) {
+            val items: Array<SmsMessage> = Telephony.Sms.Intents.getMessagesFromIntent(intent)
+            for (item in items) {Log.d("smsmsgreceiver", item.messageBody+"phno--"+item.originatingAddress)}
+        }    }
 }
